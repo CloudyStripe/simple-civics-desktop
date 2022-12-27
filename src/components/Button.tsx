43212a049d5f7617d4175c01b0d9059fe940a031
Skyler Savard <ssavard@korterra.com>
2022-12-26 20:20:14 -0600

    completed landing page and added button component. Revisit text on landing page.

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+export interface ButtonProps {
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    variant?: string;
+    size?: 'sm' | 'lg';
+    children?: React.ReactNode;
+}
+
+export const SingleButton: React.FC<ButtonProps> = ({ children, ...props }) => {
+    const { onClick, variant, size } = props;
+    return (
+        <Button
+            onClick={onClick}
+            variant={variant}
+            size={size}
+
+        >
+            {children}
+        </Button>
+    )
+}
