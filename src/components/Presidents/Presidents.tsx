86fbde4fd5481a97229294e629aae118eccfefd8
Skyler Savard <ssavard@korterra.com>
2022-12-28 21:21:24 -0600

    added hall of presidents link.

---
+++ src/components/Presidents/Presidents.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export interface PresidentProps {
+
+}
+
+export const Presidents: React.FC<PresidentProps> = () => {
+
+    return(
+        <h1>Hall of Presidents</h1>
+    )
+
+}
