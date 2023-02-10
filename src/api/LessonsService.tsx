59dd4002df93c4e0da382a2c990f4ce86d735b9a
Skyler Savard <ssavard@korterra.com>
2023-02-09 21:04:59 -0600

    displaying values. next, add decoration and real text based on booleans.

--- src/api/LessonsService.tsx
+++ src/api/LessonsService.tsx
@@ -2,7 +2,19 @@ export interface RequestInfo {
     userId: string;
 }
 
-export const getLessons = async<T,>(request: RequestInfo): Promise<T> => {
+export interface lessonInfo {
+    lesson1: boolean;
+    lesson2: boolean;
+    lesson3: boolean;
+    lesson4: boolean;
+    lesson5: boolean;
+    lesson6: boolean;
+    lesson7: boolean;
+    lesson8: boolean;
+    lesson9: boolean
+}
+
+export const getLessons = async (request: RequestInfo): Promise<lessonInfo> => {
     try {
         const { userId } = request;
         const res = await fetch(`http://localhost:3001/getLearning/${userId}`)
--- src/components/Learning/Learning.tsx
+++ src/components/Learning/Learning.tsx
@@ -3,6 +3,7 @@ import { Container, Stack } from "react-bootstrap";
 import { BootstrapCard } from "../Card/BootstrapCard";
 import ScrollContainer from 'react-indiana-drag-scroll';
 import Capital from '../../images/capital.jpeg';
+import { lessonInfo } from "../../api/LessonsService";
 import { BootstrapButton } from "../BootstrapButton";
 import { getLessons } from "../../api/LessonsService";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -12,41 +13,46 @@ import './Learning.css';
 
 export const Learning: React.FC = () => {
 
-    // const [lessonStatus, setLessonStatus] = useState([]);
+    const [lessonStatus, setLessonStatus] = useState<lessonInfo | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true)
     const { user } = useAuth0();
+
+    debugger;
     useEffect(() => {
         if (user) {
             const retrieveLessons = async () => {
                 const results = await getLessons({ userId: user.email ? user.email : '' })
-                console.log(results)
+                debugger;
+                setLessonStatus(results)
+                setIsLoading(false)
             }
             retrieveLessons();
         }
-
-    })
+    }, [])
 
     return (
         <>
-            <Container className='d-flex justify-content-between pt-5'>
-                <h1>My Learning</h1>
-            </Container>
-            <ScrollContainer horizontal={true} vertical={false}>
-                <Stack style={{ height: '60vh' }} direction="horizontal" gap={5}>
-                    {lessons.map(x => (
-                        <div className={`d-flex flex-column align-items-center cardSizing ${x["lesson-number"] === 1 ? `firstCardMargin` : ``}`}>
-                            <BootstrapCard
-                                className="cardSizing"
-                                title={x.title}
-                                body={x.body}
-                                variant="top"
-                                src={Capital} />
-                            <BootstrapButton className="learningButton" variant="secondary" size="lg">
-                                Mark Complete
-                            </BootstrapButton>
-                        </div>
-                    ))}
-                </Stack>
-            </ScrollContainer>
+            {!isLoading && (
+                <><Container className='d-flex justify-content-between pt-5'>
+                    <h1>{`My Learning`}</h1>
+                </Container><ScrollContainer horizontal={true} vertical={false}>
+                        <Stack style={{ height: '60vh' }} direction="horizontal" gap={5}>
+                            {lessons.map((x, i) => (
+                                <div className={`d-flex flex-column align-items-center cardSizing ${x["lesson-number"] === 1 ? `firstCardMargin` : ``}`}>
+                                    <BootstrapCard
+                                        className="cardSizing"
+                                        title={x.title}
+                                        body={x.body}
+                                        variant="top"
+                                        src={Capital} />
+                                    <BootstrapButton className="learningButton" variant="secondary" size="lg">
+                                        `${lessonStatus![`lesson${i + 1}` as keyof lessonInfo]}`
+                                    </BootstrapButton>
+                                </div>
+                            ))}
+                        </Stack>
+                    </ScrollContainer></>
+            )}
         </>
     )
 }
