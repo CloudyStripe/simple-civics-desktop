bceba968d7d69e7470cf05b8ebd499a8a52ed9c9
Skyler Savard <ssavard@korterra.com>
2023-02-14 20:40:54 -0600

    mapped API to buttons.

--- src/components/Learning/Learning.tsx
+++ src/components/Learning/Learning.tsx
@@ -14,42 +14,53 @@ import './Learning.css';
 export const Learning: React.FC = () => {
 
     const [lessonStatus, setLessonStatus] = useState<lessonInfo | null>(null);
-    const [isLoading, setIsLoading] = useState<boolean>(true)
-    const { user } = useAuth0();
+    const [isLoadingLessons, setLoadingLessons] = useState<boolean>(true)
+    const { user, isLoading } = useAuth0();
 
     useEffect(() => {
         if (user) {
             const retrieveLessons = async () => {
                 const results = await getLessons({ userId: user.email ? user.email : '' })
                 setLessonStatus(results)
-                setIsLoading(false)
+                setLoadingLessons(false)
             }
             retrieveLessons();
         }
-    }, [])
+
+        return () => {
+            setLoadingLessons(true)
+        }
+
+    }, [isLoading])
 
     return (
         <>
-            {!isLoading && (
+            {!isLoadingLessons && (
                 <><Container className='d-flex justify-content-between pt-5'>
                     <h1>{`My Learning`}</h1>
                 </Container><ScrollContainer horizontal={true} vertical={false}>
                         <Stack style={{ height: '60vh' }} direction="horizontal" gap={5}>
                             {lessons.map((x, i) => (
-                                <div className={`d-flex flex-column align-items-center cardSizing ${x["lesson-number"] === 1 ? `firstCardMargin` : ``}`}>
+                                <div key={x["lesson-number"]} className={`d-flex flex-column align-items-center cardSizing ${x["lesson-number"] === 1 ? `firstCardMargin` : ``}`}>
                                     <BootstrapCard
                                         className="cardSizing"
                                         title={x.title}
                                         body={x.body}
                                         variant="top"
                                         src={Capital} />
-                                    <BootstrapButton className="learningButton" variant="secondary" size="lg">
-                                        `${lessonStatus![`lesson${i + 1}` as keyof lessonInfo]}`
+                                    <BootstrapButton
+                                        className={lessonStatus![`lesson${i + 1}` as keyof lessonInfo] === true ? 'learningButtonCompleted' : 'learningButtonNotCompleted'}
+                                        variant="secondary"
+                                        size="lg">
+                                        {lessonStatus![`lesson${i + 1}` as keyof lessonInfo] === true ? 'Completed' : 'Mark Complete'}
                                     </BootstrapButton>
                                 </div>
                             ))}
                         </Stack>
                     </ScrollContainer></>
+            )}
+            {(isLoadingLessons) && (
+                <div>Loading...</div>
             )}
         </>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,11 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <Auth0Provider
-      domain="dev-s8tt4bkg.us.auth0.com"
-      clientId="A9PMM8e5nCL2D7ghFrvasfYwPNmlBY7Z"
-      redirectUri={window.location.origin}
-    >
-      <App />
-    </Auth0Provider>
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-s8tt4bkg.us.auth0.com"
+    clientId="A9PMM8e5nCL2D7ghFrvasfYwPNmlBY7Z"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>
 );
