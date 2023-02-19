20d81f29d74370174d76ed0df136ca082c1c0622
Skyler Savard <ssavard@korterra.com>
2023-02-18 23:29:55 -0600

    completed POST request.

--- src/api/LessonsService.tsx
+++ src/api/LessonsService.tsx
@@ -1,7 +1,3 @@
-export interface RequestInfo {
-    userId: string;
-}
-
 export interface lessonInfo {
     lesson1: boolean;
     lesson2: boolean;
@@ -11,15 +7,32 @@ export interface lessonInfo {
     lesson6: boolean;
     lesson7: boolean;
     lesson8: boolean;
-    lesson9: boolean
+    lesson9: boolean;
 }
 
-export const getLessons = async (request: RequestInfo): Promise<lessonInfo> => {
+export const getLessons = async (userID: string): Promise<lessonInfo> => {
     try {
-        const { userId } = request;
-        const res = await fetch(`http://localhost:3001/getLearning/${userId}`)
+        const res = await fetch(`http://localhost:3001/getLearning/${userID}`)
         const jsonRes = await res.json();
         return jsonRes;
+    }
+    catch (e: any) {
+        return e;
+    }
+}
+
+export const udpateLessons = async (userID: string, updatedLesson: lessonInfo): Promise<string> => {
+    try {
+        const res = await fetch(`http://localhost:3001/updateLearning/${userID}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updatedLesson)
+        })
+        const jsonRes = await res.json();
+        console.log(jsonRes)
+        return jsonRes
     }
     catch (e: any) {
         return e;
