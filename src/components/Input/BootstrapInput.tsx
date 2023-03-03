17438c422927866621caf01f76ef11dfd58c8a5e
Skyler Savard <ssavard@korterra.com>
2023-03-02 20:35:48 -0600

    added search functionality.

--- src/components/Input/BootstrapInput.tsx
+++ src/components/Input/BootstrapInput.tsx
@@ -5,15 +5,17 @@ export interface BootstrapInputProps {
     type: string;
     className?: string;
     label?: string;
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
     placeholder?: string;
 }
 
 export const BootstrapInput: React.FC<BootstrapInputProps> = (props) => {
-    const {type, placeholder, label, className} = props;
+    const {type, placeholder, label, onChange, className} = props;
     return(
         <Form className={className}>
             <Form.Label>{label}</Form.Label>
             <Form.Control
+                onChange={onChange}
                 type={type}
                 placeholder={placeholder}
             ></Form.Control>
