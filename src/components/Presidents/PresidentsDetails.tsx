b5f4b6ab48b0917f76c271a4422b01cdeb53f26d
Skyler Savard <ssavard@korterra.com>
2023-04-03 13:55:03 -0500

    detail responsiveness.

--- src/components/Presidents/PresidentsDetails.tsx
+++ src/components/Presidents/PresidentsDetails.tsx
@@ -27,19 +27,19 @@ export const PresidentDetails: React.FC = () => {
     }
 
     return (
-        <div className="details-container">
-            <Container className="d-flex border-bottom border-2 border-dark president-info-container">
+        <div className="detailsContainer">
+            <Container className="d-flex flex-wrap flex-wrap-md-nowrap justify-content-center justify-content-md-normal presidentInfoContainer">
                 <BootstrapImage
-                    className="m-5"
+                    className="presidentImage m-5"
                     rounded={true}
                     src={PresidentPortraitsArray[convertedId]}
-                    height='500' />
-                <Container className="d-flex align-items-center m-5">
-                    <div className="quick-facts">
+                />
+                <Container className="quickFactsContainer d-flex align-items-center mt-sm-0 m-md-5">
+                    <div className="quickFacts">
                         <h1 className="pb-3">{selectedPresident ? selectedPresident.name : ''}</h1>
                         <h2 className="pb-3">{selectedPresident ? selectedPresident.vicePresident : ''}</h2>
                         <h3 className="pb-3">{selectedPresident ? selectedPresident.serviceTerm : ''}</h3>
-                        <Container className="d-flex text-center star-container">
+                        <Container className="d-flex text-center starContainer">
                             <Star className="me-3" color="navy" size={25} />
                             <Star size={25} />
                             <Star className="ms-3" color="maroon" size={25} />
@@ -47,10 +47,11 @@ export const PresidentDetails: React.FC = () => {
                     </div>
                 </Container>
             </Container>
-            <Container className="bio-container">
+            <div className="fauxBorder mt-5"/>
+            <Container className="bioContainer">
                 <h2 className="text-center py-5">Biography</h2>
                 <p className="pb-5">{selectedPresident ? selectedPresident.biography : ''}</p>
-                <Container className="d-flex text-center star-container page-end-padding">
+                <Container className="d-flex text-center starContainer pageEndPadding">
                         <Star className="me-3" color="navy" size={25} />
                         <Star size={25} />
                         <Star className="ms-3" color="maroon" size={25} />
